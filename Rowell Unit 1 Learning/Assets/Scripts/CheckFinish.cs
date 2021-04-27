85bceb3b2f56b9d6cfb562d88098e4e304b0eb25
KennyTheGuo <kennytheguo@gmail.com>
2021-04-26 19:11:08 -0700

    Elevator?

--- Rowell Unit 1 Learning/Assets/Scripts/CheckFinish.cs
+++ Rowell Unit 1 Learning/Assets/Scripts/CheckFinish.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class CheckFinish : MonoBehaviour
 {
     public FinishEffect finishEffect;
+    public Elevate elevate;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "FinishLine")
         {
-            finishEffect.ActivateEffect(); 
-            elevate.ActivateEffect();
+            finishEffect.ActivateEffect();
+            //elevate.ActivateEffect();
         }
     }
 
--- Rowell Unit 1 Learning/Assets/Scripts/Elevate.cs
+++ Rowell Unit 1 Learning/Assets/Scripts/Elevate.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Elevate : MonoBehaviour
 {
+    public float forceMult = 10;
+//    private Rigidbody rb;
+    private bool active;
+    public float elevation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        active = false;
+    //    rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        elevation = rigidbody.velocity.magnitude;
+        if (active && elevation < 100)
+        {
+        transform.position += transform.up * forceMult *Time.deltaTime;
+    //        rb.AddForce(transform.up);
+    //        rb.AddForce(transform.up * forceMult * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            active = true;
+        }
     }
 }
