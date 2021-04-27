bda649ab56c02feca08c8612124f93e450a1f253
KennyTheGuo <kennytheguo@gmail.com>
2021-04-26 19:34:41 -0700

    basic structure

--- Rowell Unit 1 Learning/Assets/Scripts/Elevate.cs
+++ Rowell Unit 1 Learning/Assets/Scripts/Elevate.cs
@@ -5,27 +5,43 @@ using UnityEngine;
 public class Elevate : MonoBehaviour
 {
     public float forceMult = 10;
-//    private Rigidbody rb;
+    private Rigidbody rb;
     private bool active;
     public float elevation;
+    public float elevationLimit;
+    public float speedMod;
 
     // Start is called before the first frame update
     void Start()
     {
         active = false;
-    //    rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     public void Update()
     {
-        elevation = rigidbody.velocity.magnitude;
-        if (active && elevation < 100)
+        elevation = GetComponent<Rigidbody>().velocity.magnitude;
+        if (active && elevation < elevationLimit)
         {
-        transform.position += transform.up * forceMult *Time.deltaTime;
-    //        rb.AddForce(transform.up);
-    //        rb.AddForce(transform.up * forceMult * Time.deltaTime);
+    //    transform.position += transform.up * forceMult *Time.deltaTime;
+            rb.AddForce(transform.up);
+            rb.AddForce(transform.up * forceMult * Time.deltaTime);
         }
+
+    //    if (elevation > elevationLimit)
+        {
+    //        active = false;
+        }
+
+    //    if (!active && elevation > 0)
+        {
+    //    transform.position += -transform.up * forceMult *Time.deltaTime;
+    //        rb.AddForce(-transform.up);
+    //        rb.AddForce(transform.up * forceMult * Time.deltaTime * speedMod);
+    //        active = true;
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
